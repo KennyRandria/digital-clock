0e1dc1c5dcdeff92415796a2294ac332d630a3b3
KennyRandria <103984550+KennyRandria@users.noreply.github.com>
2022-06-27 23:01:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
+import { Timer, TimerClassComponent } from "./Timer/Timer";
 import { Clock, ClockClassComponent } from "./Clock/Clock";
+import { useState } from "react";
 
 function App() {
+  const [isTimer,setisTimer]=useState(false);
   return (
-    <>
-      {/*<Clock />*/}
-      <ClockClassComponent />
-    </>
+    <div>
+      <button onClick={()=>setisTimer(!isTimer)} style={{backgroundColor:"lightslategray",color:"#00f005",}}>toggle</button>
+      {isTimer?<TimerClassComponent date={new Date()}/> : <ClockClassComponent date={ new Date()}/>
+      //isTimer?<Timer date={new Date()}/> : <Clock date={new Date()}/>
+      }
+    </div>
   );
 }
 
